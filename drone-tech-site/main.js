52d2ab96258de40eacb96d0ae375429a8ac12589
fusion525 <fusion525@users.noreply.github.com>
2020-10-05 14:00:30 +0200

    form validation script has been added

---
+++ drone-tech-site/main.js
@@ -0,0 +1,29 @@
+
+function emailValidator(){
+    let email = document.getElementById("email-input").value;
+    let emailWarning = document.getElementById("email-warning");
+    const reEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (reEmail.test(email)) {
+        emailWarning.style.display = "none";
+    }
+    else {
+        emailWarning.style.display = "block";
+    }
+    console.log(email);
+}
+
+document.getElementById("email-input").addEventListener("change", emailValidator);
+
+function telValidator() {
+    let tel = document.getElementById("tel-input").value;
+    const reTel = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g;
+    let telWarning = document.getElementById("tel-warning");
+    if (reTel.test(tel)) {
+        telWarning.style.display = "none";
+    }
+    else {
+        telWarning.style.display = "block";
+    }
+}
+
+document.getElementById("tel-input").addEventListener("change", telValidator);
